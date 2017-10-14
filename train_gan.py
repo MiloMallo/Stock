114ef760554b5ebf8bf4e521bab4fb39567864f3
Nate <nharmon8@gmail.com>
2017-10-14 12:30:01 -0600

    This is what removed the adding of the missing days

--- train_gan.py
+++ train_gan.py
@@ -16,6 +16,10 @@ class TrainGan:
             #Read in file -- note that parse_dates will be need later
             df = pd.read_csv(file, index_col='Date', parse_dates=True)
             df = df[['Open','High','Low','Close','Volume']]
+            # #Create new index with missing days
+            # idx = pd.date_range(df.index[-1], df.index[0])
+            # #Reindex and fill the missing day with the value from the day before
+            # df = df.reindex(idx, method='bfill').sort_index(ascending=False)
             #Normilize using a of size num_historical_days
             df = ((df -
             df.rolling(num_historical_days).mean().shift(-num_historical_days))
@@ -23,19 +27,16 @@ class TrainGan:
             -df.rolling(num_historical_days).min().shift(-num_historical_days)))
             #Drop the last 10 day that we don't have data for
             df = df.dropna()
-            #Create new index with missing days
-            idx = pd.date_range(df.index[-1], df.index[0])
-            #Reindex and fill the missing day with the value from the day before
-            df = df.reindex(idx, method='ffill').sort_index(ascending=False)
             #Hold out the last year of trading for testing
-            df = df[252:]
+            #Padding to keep labels from bleeding
+            df = df[400:]
             #This may not create good samples if num_historical_days is a
             #mutliple of 7
             for i in range(num_historical_days, len(df), num_historical_days):
                 self.data.append(df.values[i-num_historical_days:i])
 
         self.gan = GAN(num_features=5, num_historical_days=num_historical_days,
-                        generator_input_size=100)
+                        generator_input_size=200)
 
     def random_batch(self, batch_size=128):
         batch = []
@@ -45,26 +46,31 @@ class TrainGan:
                 yield batch
                 batch = []
 
-    def train(self, print_steps=1000):
+    def train(self, print_steps=1000, save_steps=10000):
+        if not os.path.exists('./models'):
+            os.makedirs('./models')
         sess = tf.Session()
         G_loss = 0
         D_loss = 0
         sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
         for i, X in enumerate(self.random_batch(self.batch_size)):
             if i % 1 == 0:
                 _, D_loss_curr = sess.run([self.gan.D_solver, self.gan.D_loss], feed_dict=
-                        {self.gan.X:X, self.gan.Z:self.gan.sample_Z(self.batch_size, 100)})
+                        {self.gan.X:X, self.gan.Z:self.gan.sample_Z(self.batch_size, 200)})
                 D_loss += D_loss_curr
             if i % 1 == 0:
                 _, G_loss_curr = sess.run([self.gan.G_solver, self.gan.G_loss],
-                        feed_dict={self.gan.Z:self.gan.sample_Z(self.batch_size, 100)})
+                        feed_dict={self.gan.Z:self.gan.sample_Z(self.batch_size, 200)})
                 G_loss += G_loss_curr
             if i % print_steps == 0:
                 print('Step={} D_loss={}, G_loss={}'.format(i, D_loss/print_steps, G_loss/print_steps))
                 G_loss = 0
                 D_loss = 0
+            if (i+1) % save_steps == 0:
+                saver.save(sess, './models/gan.ckpt')
 
 
-
-gan = TrainGan(20, 128)
-gan.train()
+if __name__ == '__main__':
+    gan = TrainGan(20, 128)
+    gan.train()
